de01fdc13e8574160345cbf86770eca824ac976b
adrianvences9820 <adrianvences9820@gmail.com>
2023-01-23 23:29:51 -0500

    first commit

---
+++ algos.js
@@ -0,0 +1,98 @@
+// Interview Algo given to alumni Oct 2019
+
+/* 
+  You are given a list of integers. Find all the consecutive sets of 
+  integers that adds up to the sum passed in as one of the inputs.
+*/
+
+const nums1 = [2, 5, 3, 6, 7, 23, 12];
+const sum1 = 16;
+const expected1 = [
+  [2, 5, 3, 6],
+  [3, 6, 7],
+];
+
+const nums2 = [];
+const sum2 = 5;
+const expected2 = [];
+
+const nums3 = [10, 15, 20, 35, 30];
+const sum3 = 5;
+const expected3 = [];
+
+// Bonus:
+const nums4 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const sum4 = 16;
+const expected4 = [
+  [2, 5, 3, 6],
+  [3, 6, 7],
+  [3, 6, 7, 0],
+  [3, 6, 7, 0, 0],
+];
+
+// Bonus:
+const nums5 = [-2, -5, -3, -6, -7, -0, -0, -23, -12];
+const sum5 = -16;
+const expected5 = [
+  [-2, -5, -3, -6],
+  [-3, -6, -7],
+  [-3, -6, -7, -0],
+  [-3, -6, -7, -0, -0],
+];
+
+/**
+ * Finds all the sets of consecutive numbers that sum to the given target sum.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Unordered nums.
+ * @param {number} targetSum
+ * @returns {Array<Array<number>>} 2d array where each nested array is a set of
+ *    consecutive numbers that add up to the given targetSum. Consecutive in
+ *    this context means the numbers whose indexes are one after the other
+ *    only.
+ */
+
+
+// from one array we are trying to find the numbers that add up to the const sum 
+// one array breaks into new arrays that equal that sum 
+// we need a for loop to go through the array 
+// we might need a nested for loop to compare the elements to see which ones do what 
+// we need an If statement to say If this happens we want to push value into another array we are going to create 
+// we have to return new array(s)
+function findConsecutiveSums(nums, targetSum) {
+  const results = []
+
+  for (let i = 0; i < nums.length;i++) {
+    const subArray = [nums[i]]
+    let sum = nums[i] 
+    for (let j = i + 1; j < nums.length;j++) {
+      subArray.push(nums[j])
+      sum += nums[j] 
+      if(sum === targetSum) {
+        results.push(subArray) 
+        break 
+      }
+    }
+  }
+  return results 
+
+
+}
+
+console.log(findConsecutiveSums(nums5,sum5));
+
+// function pairwiseSum(arr, n)
+// {
+//     let sum = 0;
+//     for(let i = 0; i + 1 < n; i++)
+//     {
+//          
+//         // Adding the alternate numbers
+//         sum = arr[i] + arr[i + 1];
+//         document.write(sum + " ");
+//     }
+// }
+//  
+// // Driver code
+// let arr = [ 4, 10, 15, 5, 6 ];
+// let n = arr.length;
---
+++ basic13.js
@@ -0,0 +1,27 @@
+
+// Print 1-255
+// Print all the integers from 1 to 255.
+// for (let i = 1; i < 256;i++) {
+//   console.log (i)
+// }
+
+
+// Print Sum 0-255
+// Print integers from 0 to 255, and with each integer print the sum so far.
+// let sum = 0 
+// for(let i = 0; i < 256;i++) {
+  
+//   sum = i + sum
+  
+//   console.log (sum) 
+// }
+
+
+// Print Odds 1-255
+// Print all odd integers from 1 to 255.
+// let odd = 0 
+// for (let i = 0 ; i < 256 ;i++) {
+//   if (i % 2 == 1) {
+//     console.log(i)
+//   }
+// }
